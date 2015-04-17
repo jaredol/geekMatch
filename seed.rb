3fd5e21ec655f53c35348839f464df7e411a9427
Jared O <jared.oloughlin@gmail.com>
2015-04-17 12:29:09 +1000

    Add gemlock etc files

--- seed.rb
+++ seed.rb
@@ -6,6 +6,9 @@ require_relative 'profile'
 Profile.create username: 'bitGeeky77', profile_desc: 'Hi my name is...', gender: 'Male', full_name: 'Bill Smith', age: 21, image_url: 'https://randomuser.me/api/portraits/med/men/0.jpg'
 Profile.create username: 'Poindexter', profile_desc: 'Hi my name is...', gender: 'Male', full_name: 'Tim Jones', age: 32, image_url: 'https://randomuser.me/api/portraits/med/men/1.jpg'
 Profile.create username: 'Booger84', profile_desc: 'Hi my name is...', gender: 'Male', full_name: 'Mick Jaggar', age: 42, image_url: 'https://randomuser.me/api/portraits/med/men/2.jpg'
+Profile.create username: 'Lewis', profile_desc: 'Hi my name is...', gender: 'Male', full_name: 'Lewis Skolnick', age: 42, image_url: 'http://www.eastsidepatch.com/wp-content/uploads/2012/01/lewisskulnick.jpg'
+
+
 Profile.create username: 'sumChick78', profile_desc: 'Hi my name is...', gender: 'Female', full_name: 'Jenny Jones', age: 26, image_url: 'https://randomuser.me/api/portraits/med/women/67.jpg'
 Profile.create username: 'Mazza', profile_desc: 'Hi my name is...', gender: 'Female', full_name: 'Mary Jones', age: 27, image_url: 'https://randomuser.me/api/portraits/med/women/13.jpg'
 Profile.create username: 'Kezz89', profile_desc: 'Hi my name is...', gender: 'Female', full_name: 'Cameron Diaz', age: 29, image_url: 'https://randomuser.me/api/portraits/med/women/14.jpg'
